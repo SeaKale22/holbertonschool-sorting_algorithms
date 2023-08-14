f9f54e46b4eb1de4a55db25c1a620a0ac5c6cb43
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-08-14 15:56:28 -0700

    selective sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,6 +21,7 @@ void selection_sort(int *array, size_t size)
 		if (min_index != i)
 		{
 			int temp = array[i];
+
 			array[i] = array[min_index];
 			array[min_index] = temp;
 			print_array(array, size);
