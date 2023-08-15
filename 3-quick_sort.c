ab8f972277f28497be30abf14e69634f4e56d5d2
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-08-14 17:09:00 -0700

    3

---
+++ 3-quick_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+void swap(int *a, int *b);
+
+/**
+ * quick_sort- sorts an array of ints using quick sort
+ * @array: the array to be sorted
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+	int pivot = array[size - 1];
+	size_t i = -1;
+	size_t j;
+
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+		}
+	}
+	i++;
+	swap(&array[i], &array[size - 1]);
+	print_array(array, size);
+	quick_sort(array, i);
+	quick_sort(array + i + 1, size - i - 1);
+	print_array(array, size);
+}
+/**
+ * swap- swaps two elements
+ * @a: first element
+ * @b: second element
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
