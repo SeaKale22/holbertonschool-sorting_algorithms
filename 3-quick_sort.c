0b6b83253a85f69b598c2991398532b0d67a592b
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-08-14 18:21:22 -0700

    3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,8 @@
 #include "sort.h"
 
 void swap(int *a, int *b);
+void quick_sort_a(int *array, int low, int high, size_t size);
+int lomuto(int *array, int low, int high, size_t size);
 
 /**
  * quick_sort- sorts an array of ints using quick sort
@@ -9,29 +11,63 @@ void swap(int *a, int *b);
  */
 void quick_sort(int *array, size_t size)
 {
-	int pivot;
-	size_t i, j;
+	if (array && size > 1)
+		quick_sort_a(array, 0, size - 1, size);
+}
 
-	if (size < 2)
-		return;
-	pivot = array[size - 1];
-	i = -1;
+/**
+ * quick_sort_a- sorts an array of ints
+ * @array: array to be sorted
+ * @low: starting index
+ * @high: ending index
+ * @size: size of array
+ */
+void quick_sort_a(int *array, int low, int high, size_t size)
+{
+	int pivot_idx;
 
-	for (j = 0; j < size - 1; j++)
+	if (low < high)
+	{
+		pivot_idx = lomuto(array, low, high, size);
+		quick_sort_a(array, low, pivot_idx - 1, size);
+		quick_sort_a(array, pivot_idx + 1, high, size);
+	}
+}
+
+/**
+ * lomuto- partitions array using lomuto scheme
+ * @array: array
+ * @low: starting index
+ * @high: ending index
+ * @size: size of array
+ * Return: index of pivot
+ */
+int lomuto(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-			swap(&array[i], &array[j]);
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
 		}
 	}
-	i++;
-	swap(&array[i], &array[size - 1]);
-	print_array(array, size);
-	quick_sort(array, i);
-	quick_sort(array + i + 1, size - i - 1);
-	print_array(array, size);
+	if (array[i + 1] > pivot)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
 }
+
 /**
  * swap- swaps two elements
  * @a: first element
